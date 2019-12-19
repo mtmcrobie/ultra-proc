04b88fcf8eccbc3400c794dec9dc45ffcfbf1456
Avalanchian <avalanchian@gmail.com>
2019-12-19 15:31:29 +0000

    Updated Tmp and Tst modules with needed functions

--- tmp.jl
+++ tmp.jl
@@ -1,16 +1,55 @@
 module Tmp
+    # using these
+    using DelimitedFiles, Plots
 
     # add exported definitions below
-    export get_baseline_points
+    export get_files
+    export add_wavenumbers
+    export get_overlap_pixels
+    export remove_overlap
 
 
     # get user input with a custom msg
-    function get_baseline_points()
-        println("Enter desired points for baseline correction, separated by spaces:")
-        string = readline()
-        splitstring = split(string, " ")
-        return [parse(Int, i) for i in splitstring]
+    function get_files()
+        println("Getting files...")
+        org, conv = readdlm("testset.csv", ','), readdlm("conversion.csv", ',')
+        return org, conv
     end
+
+    # add wavenumbers to input file
+    function add_wavenumbers(original_file, conversion_file)
+        println("Adding wavenumbers...")
+        wavenumbers_added = hcat(conversion_file, original_file[:, 2:end])
+        return wavenumbers_added
+    end
+
+
+    # get overlap pixels
+    function get_overlap_pixels(wavenumbers_added)
+        println("Enter the last pixel on left detector and first pixel on right detector, 
+                 separated by a space: ")
+        pixelsin = split(readline())
+        lpix, rpix = parse(Int, pixelsin[1]), parse(Int, pixelsin[2])
+        lindex, rindex = findfirst(isequal(lpix), wavenumbers_added[:, 1]), findfirst(isequal(rpix), wavenumbers_added[:, 1])
+        return lindex, rindex
+    end
+        
+
+    # remove pixel overlap
+    function remove_overlap(wavenumbers_added, lindex, rindex)
+        println("Removing pixel overlap...")
+        po_removed = vcat(wavenumbers_added[1:lindex, :], wavenumbers_added[rindex:end, :])
+        writedlm("pixel_overlap_removed.csv", po_removed, ',')
+        gr()
+        porplot = plot(po_removed[2:end, 2], po_removed[2:end, findfirst(isequal(5), po_removed[1, :])])
+        png(porplot, "porplot")
+    end
+
+        
+
+
+
+
 
 
 end
